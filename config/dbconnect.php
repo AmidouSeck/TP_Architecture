d8029009133abb6813afc811c60795c8e78bed2c
Modou105 <85462247+Modou105@users.noreply.github.com>
2021-10-14 22:55:40 +0000

    Update dbconnect.php

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -1,1 +1,21 @@
+<?php
+        $pdo = null;
+        function connect_to_db()
+        {
+            $dbengine   = 'mysql';
+            $dbhost     = 'localhost';
+            $dbuser     = 'mglsi_user';
+            $dbpassword = 'passer';
+            $dbname     = 'mglsi_news';
 
+            try{
+                $pdo = new PDO("".$dbengine.":host=$dbhost; dbname=$dbname", $dbuser,$dbpassword);
+                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+         
+       return $pdo;
+            }  
+            catch (PDOException $e){
+                echo $e->getMessage();
+            }
+        }
+        ?>        
