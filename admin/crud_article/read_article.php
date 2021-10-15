670f0a791b89f368924126327514063ba4b9a147
ElhadjiMamadouCoulibaly <85483477+ElhadjiMamadouCoulibaly@users.noreply.github.com>
2021-10-15 15:47:29 +0000

    Update read_article.php

--- admin/crud_article/read_article.php
+++ admin/crud_article/read_article.php
@@ -1,5 +1,5 @@
 <?php
-// Check existence of id parameter before processing further
+
 if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     // Include config file
     require_once "../config.php";
