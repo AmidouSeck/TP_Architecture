77a623a32f2bfbb07469c29b5d5af7693c176059
ElhadjiMamadouCoulibaly <85483477+ElhadjiMamadouCoulibaly@users.noreply.github.com>
2021-10-15 15:48:05 +0000

    Update read_article.php

--- admin/crud_article/read_article.php
+++ admin/crud_article/read_article.php
@@ -11,7 +11,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
         // Bind variables to the prepared statement as parameters
         $stmt->bind_param("i", $param_id);
         
-        // Set parameters
+        // Set   parameters
         $param_id = trim($_GET["id"]);
         
         // Attempt to execute the prepared statement
